bb5e8b85b37948b9cf8d456f3e2622d8dfed3516
PedroTFX <pedro.trindade.tfx@gmail.com>
2021-11-30 18:55:06 +0000

    Cesar Enc and Dec (working) cypher

--- cypher.hs
+++ cypher.hs
@@ -5,8 +5,12 @@ isUpperCase c = c `elem` ['A', 'B'..'Z']
 isLowerCase :: Char -> Bool
 isLowerCase c = c `elem` ['a', 'b'..'z']
 
-isPunctuation :: Char -> Bool 
+isPunctuation :: Char -> Bool
 isPunctuation c = c `elem` [' ', ',', ';', '.', '?', '!', ':', '-', '(', ')']
+
+posImp :: Int -> Char -> [Char] -> Int
+posImp _ _ [] = -1
+posImp i c (a1:a) = if c == a1 then i else posImp (i+1) c a
 
 pos :: Char -> [Char] -> Int
 pos = posImp 0
@@ -14,6 +18,9 @@ pos = posImp 0
 shift :: Char -> [Char] -> Int -> Char
 shift c a n = head $ drop ((pos c a + n) `mod` length a) a
 
-posImp :: Int -> Char -> [Char] -> Int
-posImp _ _ [] = -1
-posImp i c (a1:a) = if c == a1 then i else posImp (i+1) c a
+ccript :: Int -> String -> String
+ccript x str = [shift y ['a'..'z'] x | y <- str]
+
+cesar :: String -> Int -> String -> String
+cesar encDec x str = if encDec == "enc" then ccript x str else ccript (x * (-1)) str
+
