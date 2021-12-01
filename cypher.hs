c3b605557f8a4b2bdabeba23e7335915d5a52ea2
PedroTFX <pedro.trindade.tfx@gmail.com>
2021-12-01 13:00:44 +0000

    Cesar fullly working (cesar "enc"/"dec" "YOUR_FAV_STRING_HERE" Int)

--- cypher.hs
+++ cypher.hs
@@ -12,12 +12,22 @@ isLowerCase = flip elem ['a'..'z']
 isPunctuation :: Char -> Bool 
 isPunctuation = flip elem [' ', ',', ';', '.', '?', '!', ':', '-', '(', ')']
 
+pos :: Char -> [Char] -> Int
+pos = posImp 0
+
 posImp :: Int -> Char -> [Char] -> Int
 posImp _ _ [] = -1
 posImp i c (a1:a) = if c == a1 then i else posImp (i+1) c a
 
-pos :: Char -> [Char] -> Int
-pos = posImp 0
-
 shift :: Char -> [Char] -> Int -> Char
 shift c a n = head $ drop ((pos c a + n) `mod` length a) a
+
+toUpper :: Char -> Char
+toUpper ch = toEnum (fromEnum ch + (fromEnum 'A' - fromEnum 'a'))
+
+ccript :: Char -> String -> Int -> Char
+ccript char abc x | not (isPunctuation char) = shift (if isUpperCase char then toUpper char else char) abc x 
+                  | otherwise = char
+
+cesar :: String -> String -> Int -> String
+cesar encDec str x = [ccript y ['a'..'z'] (if encDec == "enc" then x else (-1 * x)) | y <- str]
