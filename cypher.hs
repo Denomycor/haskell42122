3855ed3fce7dcbdc0fdc566bda90d353545cea00
Denomycor <afonsoresteves@hotmail.com>
2021-12-01 13:14:26 +0000

    #2 Implemented vigenere cypher

--- cypher.hs
+++ cypher.hs
@@ -21,3 +21,20 @@ pos = posImp 0
 
 shift :: Char -> [Char] -> Int -> Char
 shift c a n = head $ drop ((pos c a + n) `mod` length a) a
+
+shiftAlpha :: Char -> Int -> Char 
+shiftAlpha c n 
+    | isUpperCase c = shift c ['A'..'Z'] n
+    | isLowerCase c = shift c ['a'..'z'] n
+    | otherwise = c
+
+zipKey :: [Char] -> [Int] -> [(Char,Int)]
+zipKey [] _ = []
+zipKey _ [] = []
+zipKey (a1:a) (b1:b) 
+    | isPunctuation a1 = (a1, 0) : zipKey a (b1:b)
+    | otherwise = (a1, b1) : zipKey a b
+
+vigenere :: [Char] -> [Char] -> Bool -> [Char]
+vigenere m k op = [shiftAlpha c n | (c,n) <- zipKey m key]
+    where key = cycle $ map (\x -> pos x ['A'..'Z'] * if op then 1 else -1) k
