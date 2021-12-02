10701567dafbecb762f227ac5e55aa3a32841664
Denomycor <afonsoresteves@hotmail.com>
2021-12-02 11:18:00 +0000

    Finished main implementation

--- Main.hs
+++ Main.hs
@@ -1,16 +1,19 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Main where
 
 import System.Environment ( getArgs )
-import Cypher
+import Cypher ( cesar, subs, vigenere )
+import Control.Monad ( when )
 
 main = do
-    args <-getArgs 
+    args <-getArgs
     text <- getLine
     putStrLn $ cypher args text
+    when (not $ null text) main
 
 
-cypher :: [String] -> String -> String 
+cypher :: [String] -> String -> String
 cypher [c,d,k] t
     | c == "cesar" = cesar t (read k) (d=="enc")
     | c == "vigenere" = vigenere t k (d=="enc")
